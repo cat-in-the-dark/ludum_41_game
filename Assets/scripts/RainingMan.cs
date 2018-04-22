bbf4b2c0bbf0a0c3f77d751d6680354c6a8cdcfd
Ilya Siganov <ilya.blan4@gmail.com>
2018-04-22 15:28:20 +0600

    optimize imports

--- Assets/scripts/RainingMan.cs
+++ Assets/scripts/RainingMan.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class RainingMan : MonoBehaviour
