a4ddb32c2c37dea302b66fa36b7b50f2e5b21a5e
Ilya Siganov <ilya.blan4@gmail.com>
2018-04-22 16:05:42 +0600

    add main screen with buttons

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("Scenes/game_1");
+	}
+
+	public void ExitGame()
+	{
+		Debug.Log("Ahahaha. NO");
+	}
+}
