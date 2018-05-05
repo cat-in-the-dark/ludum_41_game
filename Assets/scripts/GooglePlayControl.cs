9fea290f4ab16eb5551462179f6d7f0951a484b5
Ilya Siganov <ilya.blan4@gmail.com>
2018-05-06 01:07:59 +0600

    add google play release

---
+++ Assets/scripts/GooglePlayControl.cs
@@ -0,0 +1,52 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class GooglePlayControl : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        var config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        
+        // silent sign in the returned user
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+        SignIn(); // ? do we realy need this?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    public void SignIn() {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated) {
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        }
+    }
+
+    void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.LogFormat("User {0} logged in.", Social.localUser.userName);
+        }
+        else
+        {
+            Debug.Log("User was not logged in.");
+        }
+    }
+
+    public void ShowLeaderboards()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated) {
+            PlayGamesPlatform.Instance.ShowLeaderboardUI();
+        }
+        else {
+            Debug.Log("Cannot show leaderboard: not authenticated");
+        }
+    }
+}
--- Assets/scripts/HighScore.cs
+++ Assets/scripts/HighScore.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using GooglePlayGames;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour
@@ -26,6 +28,25 @@ public class HighScore : MonoBehaviour
 
     public void Save(int score)
     {
+        try
+        {
+            if (PlayGamesPlatform.Instance.localUser.authenticated)
+            {
+                // Note: make sure to add 'using GooglePlayGames'
+                PlayGamesPlatform.Instance.ReportScore(score,
+                    GPGSIds.leaderboard_high_score,
+                    (bool success) =>
+                    {
+                        Debug.LogFormat("Leaderboard update success: {0} for user {1}",
+                            success, PlayGamesPlatform.Instance.localUser.userName);
+                    });
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogErrorFormat("Something is wrong with saving leaderboard results: {0}", e);
+        }
+        
         PlayerPrefs.SetInt("Score", score);
         PlayerPrefs.Save();
     }
